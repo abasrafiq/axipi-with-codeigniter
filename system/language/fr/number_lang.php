acc0c35b56826af3294330cf4aaf5458a4363e41
Stéphane Dion <stephane.dion@brand-advocate.com>
2012-08-03 16:13:22 +0200

    move to CodeIgniter 2.1.2 (first part: system folder)

--- system/language/fr/number_lang.php
+++ system/language/fr/number_lang.php
@@ -1,8 +1,10 @@
 <?php
 
-$lang['terabyte_abbr'] = 'To';
-$lang['gigabyte_abbr'] = 'Go';
-$lang['megabyte_abbr'] = 'Mo';
-$lang['kilobyte_abbr'] = 'Ko';
-$lang['bytes'] = 'Octets';
-?>
+$lang['terabyte_abbr'] = "TB";
+$lang['gigabyte_abbr'] = "GB";
+$lang['megabyte_abbr'] = "MB";
+$lang['kilobyte_abbr'] = "KB";
+$lang['bytes'] = "Bytes";
+
+/* End of file number_lang.php */
+/* Location: ./system/language/english/number_lang.php */
